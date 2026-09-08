82257b5d97d568482739a2642c09f30576e2ecb0
Qin Guan <helloqinguan@gmail.com>
2026-09-08 11:08:08 +0800

    chore: add nvidia container configs

--- modules/containers/default.nix
+++ modules/containers/default.nix
@@ -6,4 +6,11 @@
     dockerCompat = false;
     defaultNetwork.settings.dns_enabled = true;
   };
+
+  # NVIDIA CDI support for containers.
+  hardware.nvidia-container-toolkit.enable = true;
+
+  # Required for NVIDIA CDI devices with Docker.
+  virtualisation.docker.daemon.settings.features.cdi = true;
+
 }
