c5ad37384a41f08f8ef10e71ce97b7ec5f4e397a
Qin Guan <helloqinguan@gmail.com>
2026-09-09 14:55:23 +0800

    chore: update

--- hosts/qins-nixos/disko.nix
+++ hosts/qins-nixos/disko.nix
@@ -1,45 +1,45 @@
 {
-	disko.devices.disk.main = {
-		type = "disk";
-		device = "/dev/disk/by-id/nvme-CT1000P5PSSD8_22343AC8EA41";
-		content = {
-			type = "gpt";
-			partitions = {
-				ESP = {
-					priority = 1;
-					size = "1G";
-					type = "EF00";
-					content = {
-						type = "filesystem";
-						format = "vfat";
-						mountpoint = "/boot";
-						mountOptions = [ "umask=0077" ];
-					};
-				};
+  disko.devices.disk.main = {
+    type = "disk";
+    device = "/dev/disk/by-id/nvme-CT1000P5PSSD8_22343AC8EA41";
+    content = {
+      type = "gpt";
+      partitions = {
+        ESP = {
+          priority = 1;
+          size = "1G";
+          type = "EF00";
+          content = {
+            type = "filesystem";
+            format = "vfat";
+            mountpoint = "/boot";
+            mountOptions = [ "umask=0077" ];
+          };
+        };
 
-				luks = {
-					size = "100%";
-					content = {
-						type = "luks";
-						name = "cryptroot";
-						settings = {
-							allowDiscards = true;
-						};
-						content = {
-							type = "btrfs";
-							extraArgs = [ "-f" "-L" "nixos" ];
-							subvolumes = {
-								"@root" = { mountpoint = "/";		mountOptions = [ "compress=zstd" "noatime" ]; };
-								"@home" = { mountpoint = "/home";	mountOptions = [ "compress=zstd" "noatime" ]; };
-								"@nix"  = { mountpoint = "/nix";	mountOptions = [ "compress=zstd" "noatime" ]; };
-								"@swap" = { mountpoint = "/.swapvol";	swap.swapfile.size = "32G";};
-							};
-						};
-					};
-				};
-			};
-		};
-	};
+        luks = {
+          size = "100%";
+          content = {
+            type = "luks";
+            name = "cryptroot";
+            settings = {
+              allowDiscards = true;
+            };
+            content = {
+              type = "btrfs";
+              extraArgs = [ "-f" "-L" "nixos" ];
+              subvolumes = {
+                "@root" = { mountpoint = "/";    mountOptions = [ "compress=zstd" "noatime" ]; };
+                "@home" = { mountpoint = "/home";  mountOptions = [ "compress=zstd" "noatime" ]; };
+                "@nix"  = { mountpoint = "/nix";  mountOptions = [ "compress=zstd" "noatime" ]; };
+                "@swap" = { mountpoint = "/.swapvol";  swap.swapfile.size = "32G";};
+              };
+            };
+          };
+        };
+      };
+    };
+  };
 
         disko.devices.disk.sandisk = {
                 type = "disk";
--- modules/home/packages.nix
+++ modules/home/packages.nix
@@ -20,5 +20,6 @@
     tmux
 
     devenv
+    nixfmt
   ];
 }
