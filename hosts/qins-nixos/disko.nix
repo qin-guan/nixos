a02bc2881a16c2736efb626059cda7f3ba976bc4
Qin Guan <helloqinguan@gmail.com>
2026-09-09 14:57:10 +0800

    chore: update

--- hosts/qins-nixos/disko.nix
+++ hosts/qins-nixos/disko.nix
@@ -27,12 +27,37 @@
             };
             content = {
               type = "btrfs";
-              extraArgs = [ "-f" "-L" "nixos" ];
+              extraArgs = [
+                "-f"
+                "-L"
+                "nixos"
+              ];
               subvolumes = {
-                "@root" = { mountpoint = "/";    mountOptions = [ "compress=zstd" "noatime" ]; };
-                "@home" = { mountpoint = "/home";  mountOptions = [ "compress=zstd" "noatime" ]; };
-                "@nix"  = { mountpoint = "/nix";  mountOptions = [ "compress=zstd" "noatime" ]; };
-                "@swap" = { mountpoint = "/.swapvol";  swap.swapfile.size = "32G";};
+                "@root" = {
+                  mountpoint = "/";
+                  mountOptions = [
+                    "compress=zstd"
+                    "noatime"
+                  ];
+                };
+                "@home" = {
+                  mountpoint = "/home";
+                  mountOptions = [
+                    "compress=zstd"
+                    "noatime"
+                  ];
+                };
+                "@nix" = {
+                  mountpoint = "/nix";
+                  mountOptions = [
+                    "compress=zstd"
+                    "noatime"
+                  ];
+                };
+                "@swap" = {
+                  mountpoint = "/.swapvol";
+                  swap.swapfile.size = "32G";
+                };
               };
             };
           };
@@ -41,31 +66,34 @@
     };
   };
 
-        disko.devices.disk.sandisk = {
-                type = "disk";
-                device = "/dev/disk/by-id/ata-SanDisk_SDSSDH3_1T00_21503H801441";
-                content = {
-                        type = "gpt";
-                        partitions = {
-                                data = {
-                                        size = "100%";
-                                        content = {
-                                                type = "btrfs";
-                                                extraArgs = [ "-L" "SanDisk" ];
-                                                subvolumes = {
-                                                        "@data" = {
-                                                                mountpoint = "/mnt/SanDisk";
-                                                                mountOptions = [
-                                                                        "compress=zstd"
-                                                                        "noatime"
-                                                                        "nofail"
-                                                                        "x-systemd.device-timeout=10s"
-                                                                ];
-                                                        };
-                                                };
-                                        };
-                                };
-                        };
-                };
+  disko.devices.disk.sandisk = {
+    type = "disk";
+    device = "/dev/disk/by-id/ata-SanDisk_SDSSDH3_1T00_21503H801441";
+    content = {
+      type = "gpt";
+      partitions = {
+        data = {
+          size = "100%";
+          content = {
+            type = "btrfs";
+            extraArgs = [
+              "-L"
+              "SanDisk"
+            ];
+            subvolumes = {
+              "@data" = {
+                mountpoint = "/mnt/SanDisk";
+                mountOptions = [
+                  "compress=zstd"
+                  "noatime"
+                  "nofail"
+                  "x-systemd.device-timeout=10s"
+                ];
+              };
+            };
+          };
         };
+      };
+    };
+  };
 }
