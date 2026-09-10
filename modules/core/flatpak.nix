8fd556aa05e33d9e1badb1649979de3cf8a829fe
Qin Guan <helloqinguan@gmail.com>
2026-09-10 10:50:37 +0800

    chore: update

--- modules/core/flatpak.nix
+++ modules/core/flatpak.nix
@@ -29,6 +29,7 @@
         "com.slack.Slack"
         "com.spotify.Client"
         "dev.vencord.Vesktop"
+        "io.missioncenter.MissionCenter"
       ];
 
       # Optional: Automatically update Flatpaks when you run nixos-rebuild switch
