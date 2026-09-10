5bd96ecf31ad5c9b6b5669396f69a6e2371f3458
Qin Guan <helloqinguan@gmail.com>
2026-09-11 00:01:58 +0800

    chore: update

--- modules/core/nix-ld.nix
+++ modules/core/nix-ld.nix
@@ -1,5 +1,17 @@
 {
   programs.nix-ld = {
     enable = true;
+    libraries = with pkgs; [
+            zlib                                                                                                                                                                      
+zstd                                                                                                                                                                      
+stdenv.cc.cc                                                                                                                                                              
+stdenv.cc.cc.lib                                                                                                                                                          
+udev                                                                                                                                                                      
+dbus                                                                                                                                                                      
+mesa                                                                                                                                                                      
+libglvnd                                                                                                                                                                  
+curl                                                                                                                                                                      
+openssl                                                                                                                                                                   
+];
   };
 }
