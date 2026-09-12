d4ee206709e4b156968f1925882136eb2bf7b959
Qin Guan <helloqinguan@gmail.com>
2026-09-12 13:52:14 +0800

    chore: update

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
     };
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-26.05";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
