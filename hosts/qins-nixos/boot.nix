7d07801c60e8e4bd0b7fc3fa076a54b161068e69
Qin Guan <helloqinguan@gmail.com>
2026-09-16 17:03:48 +0800

    chore: update

--- hosts/qins-nixos/boot.nix
+++ hosts/qins-nixos/boot.nix
@@ -8,5 +8,20 @@
   # boot.kernelPackages = pkgs.linuxPackages_7_0;
 
   boot.plymouth.enable = true;
-  boot.plymouth.theme = "solar";
+
+  boot = {
+    # Enable "Silent boot"
+    consoleLogLevel = 3;
+    initrd.verbose = false;
+    kernelParams = [
+      "quiet"
+      "rd.udev.log_level=3"
+      "rd.systemd.show_status=auto"
+    ];
+
+    # Hide the OS choice for bootloaders.
+    # It's still possible to open the bootloader list by pressing any key
+    # It will just not appear on screen unless a key is pressed
+    loader.timeout = 0;
+  };
 }
