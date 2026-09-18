b169e185b1dfee4d4413d9cdafbde7a2db976c74
Qin Guan <helloqinguan@gmail.com>
2026-09-18 16:52:56 +0800

    chore: update

--- hosts/qins-nixos/boot.nix
+++ hosts/qins-nixos/boot.nix
@@ -4,6 +4,9 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+     boot.extraModulePackages = [ config.boot.kernelPackages.lenovo-legion-module ];                                                                                                                                                        
+     boot.kernelModules = [ "legion-laptop" ];                                                                                                                                                                                              
+
   # Kernel 7.2 breaks the current NVIDIA driver because of strncpy deprecation.
   # boot.kernelPackages = pkgs.linuxPackages_7_0;
 
