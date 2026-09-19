8c6856b3cc3e1c2e6a0666ce5db54c5d64191410
Qin Guan <helloqinguan@gmail.com>
2026-09-19 22:34:23 +0800

    chore: update

--- modules/core/appimage.nix
+++ modules/core/appimage.nix
@@ -1,5 +1,14 @@
-{ ... }: 
+{ pkgs, ... }:
 {
   programs.appimage.enable = true;
   programs.appimage.binfmt = true;
+
+  binfmt.registrations.appimage = {
+    wrapInterpreterInShell = false;
+    interpreter = "${pkgs.appimage-run}/bin/appimage-run";
+    recognitionType = "magic";
+    offset = 0;
+    mask = ''\xff\xff\xff\xff\x00\x00\x00\x00\xff\xff\xff'';
+    magicOrExtension = ''\x7fELF....AI\x02'';
+  };
 }
