2d3fa57620201257edc739207bdfc0bf24e6adb1
Qin Guan <helloqinguan@gmail.com>
2026-09-22 02:11:26 +0800

    chore: update

--- modules/home/mise.nix
+++ modules/home/mise.nix
@@ -4,7 +4,7 @@
     enable = true;
     # Skip the upstream test suite so CI and local rebuilds share the same
     # cached derivation. The binary is what we install.
-    package = inputs.mise.packages.${pkgs.system}.default.overrideAttrs (_: {
+    package = inputs.mise.packages.${pkgs.stdenv.hostPlatform.system}.default.overrideAttrs (_: {
       doCheck = false;
     });
   };
