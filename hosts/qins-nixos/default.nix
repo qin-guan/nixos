ccae26b0adb7dbb516b6e736a37c78a565ee062e
Qin Guan <helloqinguan@gmail.com>
2026-09-23 18:57:45 +0800

    chore: update

--- hosts/qins-nixos/default.nix
+++ hosts/qins-nixos/default.nix
@@ -37,7 +37,7 @@
   services.udev.extraRules = ''
     # Keychron Launcher WebHID access
     KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="3434", ATTRS{idProduct}=="d044", MODE="0660", GROUP="input", TAG+="uaccess"
-    ''
+  '';
 
 
   system.stateVersion = "26.05";
