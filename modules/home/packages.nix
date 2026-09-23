f57394d62980df4b15097df422f82f57def5968f
Qin Guan <helloqinguan@gmail.com>
2026-09-23 10:20:04 +0800

    chore: update

--- modules/home/packages.nix
+++ modules/home/packages.nix
@@ -22,14 +22,20 @@
     devenv
     nixfmt
 
+    # Ghostty
     inputs.ghostty.packages.${pkgs.stdenv.hostPlatform.system}.default
 
+    # nono
     inputs.llm-agents.packages.${pkgs.stdenv.hostPlatform.system}.nono
+
     inputs.llm-agents.packages.${pkgs.stdenv.hostPlatform.system}.opencode2
-    inputs.llm-agents.packages.${pkgs.stdenv.hostPlatform.system}.cursor-agent
 
     inputs.llm-agents.packages.${pkgs.stdenv.hostPlatform.system}.grok
     inputs.llm-agents.packages.${pkgs.stdenv.hostPlatform.system}.grok-bot
+    inputs.llm-agents.packages.${pkgs.stdenv.hostPlatform.system}.cursor-agent
+    
+    inputs.llm-agents.packages.${pkgs.stdenv.hostPlatform.system}.claude
+    inputs.llm-agents.packages.${pkgs.stdenv.hostPlatform.system}.codex
 
     inputs.llm-agents.packages.${pkgs.stdenv.hostPlatform.system}.gitbutler
     inputs.llm-agents.packages.${pkgs.stdenv.hostPlatform.system}.but
